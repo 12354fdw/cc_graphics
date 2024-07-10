c4c8c88205028047c2265611d313244ffa81300b
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-10 17:49:52 +0700

    Update manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -1,7 +1,8 @@
 return {
-    "button_demo.lua",
-    "frame_demo.lua",
-    "graphics.lua",
-    "nested_demo.lua",
-    "text_demo.lua"
+    ForcedDir = false,
+    {"button_demo.lua",false,"/"},
+    {"frame_demo.lua",false,"/"},
+    {"graphics.lua",false,"/"},
+    {"nested_demo.lua",false,"/"},
+    {"text_demo.lua",false,"/"}
 }
